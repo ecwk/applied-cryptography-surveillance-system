ac6f5798ce2f34afc45139779a28e3053e0c7ee0
cNoside <edisonchanwk@gmail.com>
2022-02-11 18:30:19 +0800

    Modified server.py to use models

--- src/server/server.py
+++ src/server/server.py
@@ -5,27 +5,18 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler, TLS_FTPHandler
 from pyftpdlib.servers import FTPServer
 
+from models import UserModel
+
 os.chdir(pathlib.Path(__file__).parent.resolve())
 
 BASE_DIR = os.path.join(os.getcwd(), 'data')
 ANONYMOUS_DIR = os.path.join(os.getcwd(), 'data/anonymous')
 ADDRESS = ('127.0.0.1', 2121)
 
-USERS = [
-  {
-    'username': 'cam-001',
-    'password': ''
-  },
-  {
-    'username': 'cam-002',
-    'password': ''
-  }
-]
-
-
 def main():
+  users = UserModel.getAll()
   authorizer = DummyAuthorizer()
-  for user in USERS:
+  for user in users:
     USER_DIR = os.path.join(BASE_DIR, user['username'])
     if not os.path.exists(USER_DIR):
       os.makedirs(USER_DIR)
@@ -48,5 +39,9 @@ def main():
 
   server.serve_forever()
 
+print(UserModel.getAll())
 
 main()
+
+
+
