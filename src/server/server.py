2d669408f664695f6fc59a89d9d1f99509004a9c
cNoside <edisonchanwk@gmail.com>
2022-02-11 18:15:44 +0800

    Added ftp server user system

--- src/server/server.py
+++ src/server/server.py
@@ -7,13 +7,36 @@ from pyftpdlib.servers import FTPServer
 
 os.chdir(pathlib.Path(__file__).parent.resolve())
 
+BASE_DIR = os.path.join(os.getcwd(), 'data')
 ANONYMOUS_DIR = os.path.join(os.getcwd(), 'data/anonymous')
 ADDRESS = ('127.0.0.1', 2121)
+
+USERS = [
+  {
+    'username': 'cam-001',
+    'password': ''
+  },
+  {
+    'username': 'cam-002',
+    'password': ''
+  }
+]
 
 
 def main():
   authorizer = DummyAuthorizer()
-  authorizer.add_user('cam-001', '12345', f'data/cam-001', perm='elradfmwMT')
+  for user in USERS:
+    USER_DIR = os.path.join(BASE_DIR, user['username'])
+    if not os.path.exists(USER_DIR):
+      os.makedirs(USER_DIR)
+
+    authorizer.add_user(
+      user['username'],
+      user['password'],
+      USER_DIR, 
+      perm='elradfmw'
+    )
+
   authorizer.add_anonymous(ANONYMOUS_DIR, perm='elradfmwMT')
 
   handler = FTPHandler
