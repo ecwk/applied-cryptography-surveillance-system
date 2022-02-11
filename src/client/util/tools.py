68f13f219d702f73f56c2cc2f6af22e8cf6f2b03
cNoside <edisonchanwk@gmail.com>
2022-02-11 18:15:22 +0800

    Added util/tools.py

---
+++ src/client/util/tools.py
@@ -0,0 +1,51 @@
+import re
+import os
+from msvcrt import getch
+import sys
+
+
+def clearConsole():
+  """Clears console on Linux or Windows"""
+  if os.name == "nt": # windows
+    os.system("cls")
+  elif os.name == "posix": # mac
+    os.system("clear")
+  else:
+    raise OSError("Your operating system is not supported.")
+
+
+def hideStr(string):
+  hiddenStr = ""
+  for char in string:
+    hiddenStr += "*"
+  return hiddenStr
+
+
+def hiddenInput(output=''):
+  print(output, end="")
+  value = []
+  inputLen = 0
+  while True:
+    char = getch()
+    decodedChar = char.decode()
+    regex = '[0-9A-Za-z!@#$%^&*()]'
+    match = re.match(regex, decodedChar)
+    if match:
+      match = match.group()
+      value.append(match)
+      print('*', end="")
+      inputLen += 1
+    else:
+      pass
+
+    if char == b'\x08':
+      if not inputLen == 0:
+        sys.stdout.write('\b \b')
+        value.pop()
+        inputLen -= 1
+    elif char == b'\r':
+      processedVal = ''
+      for char in value:
+        processedVal += char
+      print('')
+      return processedVal
