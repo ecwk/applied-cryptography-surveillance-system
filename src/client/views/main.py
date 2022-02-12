71049e765b38cdb6eb905a6ffe39c8fcbdfc003b
cNoside <edisonchanwk@gmail.com>
2022-02-12 17:22:50 +0800

    Modified main controller's view

--- src/client/views/main.py
+++ src/client/views/main.py
@@ -2,15 +2,23 @@ from util.config import config
 
 def main(**props):
   cameraOn = props['cameraOn']
+  privKey = props['privKey']
+    
   cameraOnStr = {
     'text': [f'Camera On: ', str(cameraOn)],
   }
-
   if cameraOn:
     cameraOnStr['color'] = [None, [(0, 255, 0), None]]
   else:
     cameraOnStr['color'] = [None, [(255, 0, 0), None]]
 
+  privKeyStr = {
+    'text': [f'Private Key: ', 'Unlocked' if privKey else 'Locked'],
+  }
+  if privKey:
+    privKeyStr['color'] = [None, [(0, 255, 0), None]]
+  else:
+    privKeyStr['color'] = [None, [(255, 0, 0), None]]
 
   return {
     'title': [
@@ -22,7 +30,8 @@ def main(**props):
     ],
     'bodyHeader': [{ 'text': ['Status'] }],
     'body': [
-      cameraOnStr
+      cameraOnStr,
+      privKeyStr
     ]
   }
 
